698cbf75296b339a14ff3138c920c5681f315d7e
Sehe2000 <sebastianhe2004@hotmail.com>
2024-10-15 14:23:09 +0200

    start
    et utgangspunkt?

--- Battle/Battle.js
+++ Battle/Battle.js
@@ -1,1 +1,31 @@
-Get
+// Character class to create character objects
+class Character {
+    constructor(name, health, attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    // Method to attack another character
+    attack(target) {
+        console.log(`${this.name} attacks ${target.name} for ${this.attackPower} damage!`);
+        target.takeDamage(this.attackPower);
+    }
+
+    // Method to take damage
+    takeDamage(damage) {
+        this.health -= damage;
+        console.log(`${this.name} now has ${this.health} health left.`);
+        
+        if (this.health <= 0) {
+            console.log(`${this.name} has been defeated!`);
+        }
+    }
+}
+
+// Create two characters
+const character1 = new Character('Warrior', 100, 20);
+const character2 = new Character('Mage', 80, 15);
+
+// Character 1 attacks Character 2
+character1.attack(character2);
